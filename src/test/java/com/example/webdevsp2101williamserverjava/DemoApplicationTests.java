dd43058a34b6b914e452b06155007fc474f8b2b2
unknown <weiweiliu2018@outlook.com>
2021-01-31 15:46:59 +0800

    first commit

---
+++ src/test/java/com/example/webdevsp2101williamserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevsp2101williamserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
